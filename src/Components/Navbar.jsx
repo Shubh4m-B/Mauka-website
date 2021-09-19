02a429c7dadd1f71c4cee11ca82fa7f9bfdbcf3a
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-09-19 18:35:17 +0530

    Add positoning to the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import "../Styles/Navbar.css"
 
 export default function Navbar() {
     return (
         <div className="Navbar">
             <div className="Navbar-logo">
                 <img className="Navbar-img" src="Images/mauka-blue.png" alt="Logo" />
-                <h1 className="Navbar-title">Navbar</h1>
+                <h1 className="Navbar-title">Mauka</h1>
             </div>
             <div className="Navbar-links">
-                <li>Bootcamp</li>
-                <li>Demo</li>
-                <li>Blog</li>
-                <li>Cohorts</li>
-                <li>Talk to us</li>
-                <li>Login</li>
+                <li><h3>Bootcamp</h3></li>
+                <li><h3>Demo</h3></li>
+                <li><h3>Blog</h3></li>
+                <li><h3>Cohorts</h3></li>
+                <li><h3>Talk to us</h3></li>
+                <li><h3>Login</h3></li>
                 <button className="Navbar-button">
-                    Enroll Now
+                    <h3>Enroll Now</h3>
                 </button>
             </div>
         </div>
