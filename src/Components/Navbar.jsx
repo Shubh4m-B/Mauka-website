417753b5e2ffdb2c10a089b99437d3c6f217a7ac
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-09-19 18:09:34 +0530

    Define structure of the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div className="Navbar">
+            <div className="Navbar-logo">
+                <img className="Navbar-img" src="Images/mauka-blue.png" alt="Logo" />
+                <h1 className="Navbar-title">Navbar</h1>
+            </div>
+            <div className="Navbar-links">
+                <li>Bootcamp</li>
+                <li>Demo</li>
+                <li>Blog</li>
+                <li>Cohorts</li>
+                <li>Talk to us</li>
+                <li>Login</li>
+                <button className="Navbar-button">
+                    Enroll Now
+                </button>
+            </div>
         </div>
     )
 }
